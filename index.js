c5619feb83dfbfa7d62cb2eb36a0957a49189dcb
Geovanni jorge da cruz <geovannijorge081@gmail.com>
2023-07-11 12:25:50 -0300

    rascunho da calculadora

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Ola")
+const prompt = require('prompt-sync')();
+var num1 = Number(prompt('Digite o primeiro valor: '));
+
+var op = prompt('Digite a operação que deseja realiza: ');
+
+var num2 = Number(prompt('Digite o segundo valor: '));
+
+var resultado = 'operação invalida';
+if (op === '+') {
+    resultado = (num1 + num2);
+}
+if (op === '-') {
+    resultado = (num1-num2);
+}
+if (op === '*') {
+    resultado = (num1*num2);
+}
+if (op === '/') {
+    resultado = (num1/num2)
+}
+if (resultado === 'operação invalida') {
+    console.log('operação invalida')
+}
+if (resultado !== 'operação invalida') {
+    console.log('O resultado é', resultado);    
+}
+
